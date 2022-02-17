ec46b7e59db68f57041571acecf4fe841eadae52
Jaromír <xbrezi18@gmail.com>
2022-02-17 17:49:44 +0100

    Vyhazeni redundantnich prop

--- src/Components/Utilities/UtilComponents/ArrowSVG.tsx
+++ src/Components/Utilities/UtilComponents/ArrowSVG.tsx
@@ -28,7 +28,7 @@ export const ArrowSVG : FC<ArrowSVGComponentProps> = (props) => {
     return (
         <g visibility={props.visible} onClick={props.onClick} transform={`translate(${props.coordinates.x} ${props.coordinates.y}) scale(${(props.scale ?? 1) * 0.7} ${(props.scale ?? 1)* 0.7}) rotate(${directionConvertor()})`}>
             <g transform={`translate(10 0)`}>
-                <path d={`M 0 0 L 0 -5 L 25 -5 L 25 -10 L 40 0 L 25 10 L 25 5 L 0 5 L 0 0 Z`} fill={"none"}  stroke={"rgb(0, 0, 0)"} className={styles.arrow}  strokeMiterlimit={"10"} pointerEvents={"all"}></path>
+                <path d={`M 0 0 L 0 -5 L 25 -5 L 25 -10 L 40 0 L 25 10 L 25 5 L 0 5 L 0 0 Z`} className={styles.arrow} ></path>
             </g>
         </g>
     )
