1e1e56a6d1febd348db0de79bf8c876233865ba3
Jaromír <xbrezi18@gmail.com>
2022-02-27 21:09:32 +0100

    menu transition fill change

--- src/Components/Icons.tsx
+++ src/Components/Icons.tsx
@@ -1,5 +1,5 @@
 
-import placeIcon from "./PetriNets/icons/petri-spot.svg"
+import placeIcon from "./PetriNets/icons/petri-place.svg"
 import transitionIcon from "./PetriNets/icons/petri-transition.svg"
 
 export const MenuIcons = {
