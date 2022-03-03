6eccc0ba93ac6cbd9a677627b777a14d8eb3002b
Trautenberk <brjarda@gmail.com>
2022-03-03 17:47:39 +0100

    Oprava v craco.config

--- craco.config.js
+++ craco.config.js
@@ -6,10 +6,6 @@ module.exports = {
       const wasmExtensionRegExp = /\.wasm$/;
       webpackConfig.resolve.extensions.push('.wasm');
 
-      const mjsExtensionRegExp = /\/mjs$/;
-      webpackConfig.resolve.extensions.push('.mjs');
-      
-
       webpackConfig.module.rules.forEach((rule) => {
         (rule.oneOf || []).forEach((oneOf) => {
           if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
@@ -18,11 +14,6 @@ module.exports = {
         });
       });
 
-      const mjsLoader = {
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: "javascript/auto"}
-
       const wasmLoader = {
         test: /\.wasm$/,
         exclude: /node_modules/,
@@ -30,7 +21,6 @@ module.exports = {
       };
 
       addBeforeLoader(webpackConfig, loaderByName('file-loader'), wasmLoader);
-      addBeforeLoader(webpackConfig,loaderByName("file-loader"), mjsLoader);
 
       return webpackConfig;
     },
