b5cc450b4cec8e24435a0373e2fa2445ed01cf29
Trautenberk <brjarda@gmail.com>
2022-03-08 18:12:00 +0100

    place implementation

--- src/Simulator/PetriNets/Place.cpp
+++ src/Simulator/PetriNets/Place.cpp
@@ -1,16 +1,28 @@
 #include "Place.hpp"
+#include <iostream>
 
-
-Place::Place() : SimObject()
+Place::Place(string label, int tokens) : SimObject()
 {
-
+    this->_label = label;
+    this->_tokens = tokens; 
 }
 
+void Place::addTokens(int cnt)
+{
+    this->_tokens += cnt;  
+}
+
+void Place::removeTokens(int cnt)
+{
+    if (this->_tokens - cnt < 0)
+    {
+        cerr << "Cannot remove more tokens then there is present, Place Id: " << this->id << endl;
+        throw new exception();
+    }
+}
 
 Place::~Place()
-{
-
-}
+{}
 
 string Place::getObjType()
 {
--- src/Simulator/PetriNets/Place.hpp
+++ src/Simulator/PetriNets/Place.hpp
@@ -5,10 +5,15 @@ using namespace std;
 
 class Place : SimObject{
     public :
-        string label;
-    
-        Place();
+        string label() {return _label;};
+        int tokens() {return _tokens;};
+        void removeTokens(int cnt);
+        void addTokens(int cnt);
+        Place(string label, int tokens);
         ~Place();
         string getObjType();
+    private:
+        string _label;
+        int _tokens;
 };
 
